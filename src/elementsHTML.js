ac3308e58af0625bb7c7641524a9a10d0c9e5e5a
eduardob7 <edu77barroso@hotmail.com>
2023-08-16 14:04:50 -0300

    add mode filer

--- src/elementsHTML.js
+++ src/elementsHTML.js
@@ -1,4 +1,13 @@
 const app = document.querySelector("#app");
 const minutes = document.querySelector(".timer #minutes");
+const seconds = document.querySelector(".timer #seconds");
+const lightModeBtn = document.querySelector("#lightMode-Btn");
+const darkModeBtn = document.querySelector("#darkMode-Btn");
 
-export const elementsHTML = { app, minutes };
+export const elementsHTML = {
+  app,
+  minutes,
+  seconds,
+  lightModeBtn,
+  darkModeBtn,
+};
