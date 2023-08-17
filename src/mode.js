4e2d281e56754856574b5f204b7239efe18c1fb3
eduardob7 <edu77barroso@hotmail.com>
2023-08-17 12:19:18 -0300

    updade mode.js (comentarios)

--- src/mode.js
+++ src/mode.js
@@ -15,18 +15,25 @@ const {
 export class Mode {
   constructor(element) {
     element.onclick = () => {
+      // logica do icone de ligth/dark
       lightModeBtn.classList.toggle("hidden");
       darkModeBtn.classList.toggle("hidden");
-      // ...
+
+      // logica da versao dark mode do timer
       main.classList.toggle("backgroundDark");
       minutes.classList.toggle("darkmode-defaultColor");
       seconds.classList.toggle("darkmode-defaultColor");
-      // ...
+
+      // logica da versao dark mode dos icones dos controles do timer
       allControllers.classList.toggle("darkmode-defaultColor-Btns");
+
+      // logica da versao dark mode dos cards(backgroud e icones)
       cardsGroup.forEach((card) => {
         card.classList.toggle("darkmode-defaultColor-Cards");
         card.classList.toggle("darkmode-defaultColor-Cards-svg");
       });
+
+      // logica da versao dark mode do slide
       slideVolumeGroup.forEach((slide) => {
         if (!darkModeBtn.classList.contains("hidden")) {
           slide.classList.remove("slide-volume");
