a62dab57989c03162f29b87c638947697cb59b0f
eduardob7 <edu77barroso@hotmail.com>
2023-08-18 10:59:05 -0300

    Timer.js add

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -1,3 +1,20 @@
-import { Controlls } from "./controlls.js";
+// Logica dos botoes de controle do Timer
+import { elementsHTML } from "./elementsHTML.js";
 
-new Controlls();
+const { playAndpause, playBtn, pauseBtn } = elementsHTML;
+
+// Logica dos botões play/pause
+export class Controlls {
+  constructor() {
+    playAndpause.onclick = () => {
+      playBtn.classList.toggle("hidden");
+      pauseBtn.classList.toggle("hidden");
+    };
+  }
+}
+
+export class Timer extends Controlls {
+  constructor() {
+    super();
+  }
+}
