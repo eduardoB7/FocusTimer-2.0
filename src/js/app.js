d8b3321cb9b2b537fffc6f51e0ed847b7132f811
eduardob7 <edu77barroso@hotmail.com>
2023-08-18 11:10:40 -0300

    app.js update

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
 // arquivo que roda as classes e faz todo o app funcionar
 
 import { Mode } from "./Mode.js";
-import { Controlls } from "./controlls.js";
-
-// Funcionalidade do modo Ligth/Dark
+import { Timer } from "./Timer.js";
 export class App {
   constructor() {
     new Mode();
-    new Controlls();
+    new Timer();
   }
 }
