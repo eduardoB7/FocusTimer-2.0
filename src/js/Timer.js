5cd72d8fa404492cfec1e2b254f73ea46d346cbb
eduardob7 <edu77barroso@hotmail.com>
2023-08-19 15:07:46 -0300

    update of timer

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -1,50 +1,54 @@
 // Logica dos botoes de controle do Timer
 import { elementsHTML } from "./elementsHTML.js";
 
-const { playAndpause, playBtn, pauseBtn, stopBtn } = elementsHTML;
+// Elementos necessarios no controle/timer
+const {
+  playAndpause,
+  playBtn,
+  pauseBtn,
+  stopBtn,
+  addTimerBtn,
+  subtractTimerBtn,
+  minutes,
+  seconds,
+} = elementsHTML;
+let minutesCount = parseFloat(minutes.textContent);
 
-// Logica dos controles do timer
-export class Controlls {
-  constructor() {
-    this.playAndpause();
-    this.stop();
-  }
-
-  //   metodo que alterna o botao play/pause
-  playAndpause() {
-    playAndpause.onclick = () => {
-      playBtn.classList.toggle("hidden");
-      this.play();
-      pauseBtn.classList.toggle("hidden");
-    };
-  }
-
-  //   metodo que da play no timer
-  play() {
-    if (playBtn.classList.contains("hidden")) {
-      return console.log("contador play teste");
-    }
-    if (!playBtn.classList.contains("hidden")) {
-      return console.log("contador pause teste");
-    }
-  }
-
-  //   metodos que aciona o stop do timer
-  stop() {
-    stopBtn.onclick = () => {
-      if (!playBtn.classList.contains("hidden")) {
-        console.log("stop clicado");
-      } else {
-        playBtn.classList.remove("hidden");
-        pauseBtn.classList.add("hidden");
-        console.log("timer resetado");
+// Logica das funcionalidades do timer
+export class Timer {
+  constructor() {}
+  // metodo do contador
+  countdown() {
+    let secondsCount = parseFloat(seconds.textContent);
+    const timerOut = setInterval(() => {
+      secondsCount--;
+      if (secondsCount < 0) {
+        secondsCount = 59;
+        minutesCount--;
       }
-    };
+      seconds.textContent = String(secondsCount).padStart(2, "0");
+      minutes.textContent = String(minutesCount).padStart(2, "0");
+    }, 1000);
   }
 }
 
-export class Timer extends Controlls {
+// logica dos controles do Timer
+export class Controlls extends Timer {
   constructor() {
     super();
+    this.play();
   }
+
+  //   metodo que alterna o botao play/pause
+  playAndpause() {}
+
+  //   metodo que da play no timer
+  play() {
+    playBtn.onclick = () => {
+      this.countdown();
+    };
+  }
+
+  //   metodos que aciona o stop do timer
+  stop() {}
 }
