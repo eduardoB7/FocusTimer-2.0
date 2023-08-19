86778f70bef86bc9e1c510d7bd8d8ea4825c3164
eduardob7 <edu77barroso@hotmail.com>
2023-08-18 21:26:05 -0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
 // arquivo que roda as classes e faz todo o app funcionar
 
 import { Mode } from "./Mode.js";
-import { Timer } from "./Timer.js";
+import { Controlls } from "./Timer.js";
 export class App {
   constructor() {
     new Mode();
-    new Timer();
   }
 }
